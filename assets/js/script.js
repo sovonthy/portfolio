0f0d8937bb8f42bdf0108c12cefb47613dd6f102
sovonthy <sovonthy.leng@hatthabank.com>
2023-04-20 14:59:16 +0700

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,19 +56,6 @@ $(document).ready(function () {
 
 });
 
-document.addEventListener('visibilitychange',
-    function () {
-        if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
-        }
-        else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
-        }
-    });
-
-
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
     strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
